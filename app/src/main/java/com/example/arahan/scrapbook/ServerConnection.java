ef59cb0e671e51e22f791ea3b68a2164f0302d28
Ashish Pawar <ashishpawar2015.ap@gmail.com>
2017-09-15 16:01:55 +0530

    Server connection don

---
+++ app/src/main/java/com/example/arahan/scrapbook/ServerConnection.java
@@ -0,0 +1,104 @@
+package com.example.arahan.scrapbook;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
+import com.example.arahan.scrapbook.database.DBHelper;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Ashish Pawar(ashishpawar2015.ap@gmail.com) on 15/9/17.
+ */
+
+public class ServerConnection {
+    private AQuery aQuery;
+    private Context context;
+    String send = "http://";
+    String fetch = "http://";
+
+    public ServerConnection(Context context) {
+        aQuery = new AQuery(context);
+        this.context = context;
+    }
+
+    public void sendData(HashMap<String, Object> sendParams) {
+
+        aQuery.ajax(send, sendParams, JSONObject.class, new AjaxCallback<JSONObject>() {
+            @Override
+            public void callback(String url, JSONObject object, AjaxStatus status) {
+                super.callback(url, object, status);
+                int code = 0;
+                if (object != null) {
+                    try {
+                        code = object.getInt("ResultSet");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Toast.makeText(context, "Failed to send data", Toast.LENGTH_SHORT).show();
+                }
+
+                if (status.getCode() == 200) {
+                    if (code == 0) {
+                        Toast.makeText(context, "Data sent successfully", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    public void FetchData() {
+        Map<String, Object> fetchParams = new HashMap<>();
+
+        aQuery.ajax(fetch, fetchParams, JSONObject.class, new AjaxCallback<JSONObject>() {
+            @Override
+            public void callback(String url, JSONObject object, AjaxStatus status) {
+                super.callback(url, object, status);
+                int length = 0;
+                ArrayList<HashMap<String, String>> queryValues = new ArrayList<HashMap<String, String>>();
+                if (object != null) {
+                    try {
+                        String data = object.getString("Data");
+                        length = object.getInt("length");
+
+                        JSONObject reader = new JSONObject(data);
+
+                        for (int i = 0; i < length; i++) {
+                            JSONObject obj = reader.getJSONObject(String.valueOf(i));
+                            HashMap<String, String> map = new HashMap<String, String>();
+                            map.put("name", obj.getString("name"));
+                            map.put("email", obj.getString("email"));
+                            map.put("hobbies", obj.getString("hobbies"));
+                            map.put("best_friend", obj.getString("best_friend"));
+                            map.put("contact", obj.getString("contact"));
+                            map.put("crush", obj.getString("crush"));
+
+                            queryValues.add(map);
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Toast.makeText(context, "Failed to fetch data", Toast.LENGTH_SHORT).show();
+                }
+                if (status.getCode() == 200) {
+                    DBHelper helper = new DBHelper(context);
+                    helper.truncateTable();
+                    for (int i = 0; i < length; i++)
+                        helper.insertUser(queryValues.get(i));
+                    Toast.makeText(context, "Data fetched successfully", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/arahan/scrapbook/ThirdActivity.java
@@ -0,0 +1,91 @@
+package com.example.arahan.scrapbook;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.arahan.scrapbook.database.DBHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static android.R.attr.data;
+import static android.R.id.list;
+import static android.system.Os.remove;
+import static com.example.arahan.scrapbook.R.id.listview;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    ListView listView;
+    DBHelper dbHelper = new DBHelper(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        listView = (ListView) findViewById(listview);
+
+        final ArrayList<String> values = new ArrayList<>();
+        ArrayList<HashMap<String, String>> data = dbHelper.getAllUsers();
+        for (HashMap<String, String> hm: data) {
+            String output = "";
+            output += hm.get("name") + "\n";
+            output += hm.get("email") + "\n";
+            output += hm.get("hobbies") + "\n";
+            output += hm.get("best_friend") + "\n";
+            output += hm.get("contact") + "\n";
+            output += hm.get("crush") + "\n";
+            values.add(output);
+        }
+
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, values);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+
+}
